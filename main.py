78b974a6f242067b289022a96cc0e474f23e529f
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-12-01 00:47:48 +0300

    Try2

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ app = FastAPI()
 async def main_page() -> dict:
     return {"message": "Главная страница"}
 
+@app.get("/user/admin")
+async def admin_page() -> dict:
+    return {"message": "Вы вошли как администратор"}
 
-@app.get("/id")
+@app.get("/user")
 async def info_page(username: str, age: int) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-
+    return {"info":f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
 @app.get("/user/{user_id}")
 async def user_page(user_id: int) -> dict:
     return {"message": f"Вы вошли как пользователь №-{user_id}"}
 
 
-@app.get("/user/admin")
-async def admin_page() -> dict:
-    return {"message": "Вы вошли как администратор"}
+
